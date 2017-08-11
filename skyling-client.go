a99379fe85639d19e54eed0249ddf8f5c25d78a8
Juan Martin Enriquez <juanenriquez@gmail.com>
2017-08-11 01:22:58 -0300

    Output improvements

--- skyling-client.go
+++ skyling-client.go
@@ -5,17 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"github.com/juan88/go-rainbow"
 )
 
 func main() {
-
-	//url := ""
-	
 	filenamePtr := flag.String("file", "", "The filename to send to the Skyling server. Formats: Aligned FASTA, Clustal (and Clustal-like), PSI-BLAST, PHYLIP, Selex, GCG/MSF, STOCKHOLM format, UC Santa Cruz A2M (alignment to model)")
 	heightPtr := flag.String("height", "info_content_all", "Letter height server param. Available options are: info_content_all (default), info_content_above, score")
 	processingPtr := flag.String("processing", "info_content_all", "Processing mode. Available options are: observed (default), weighted, hmm, hmm_all")
 	fragPtr := flag.String("frag", "full", "Fragment handling. Available options: full, frag")
-
 	flag.Parse()
 	
 	if(len(*filenamePtr) == 0) {
@@ -24,10 +21,11 @@ func main() {
 	}
 
 	params := map[string]string{
-		"processing": "observed",
-		"letter_height": "info_content_all",
-		"frag":	"full",
+		"processing": *processingPtr,
+		"letter_height": *heightPtr,
+		"frag":	*fragPtr,
 	}
 	
 	client.GenerateLogo(*filenamePtr, params)
+	fmt.Println(rainbow.Green("Logo generated successfully!"))
 }
